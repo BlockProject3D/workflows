914f346638b602c980f861a8e0bc76d5a87140f1
Yuri Edward <yuri6037@outlook.com>
2024-07-31 16:44:51 +0200

    Added initial version of CodeCov workflow

---
+++ scripts/run-coverage.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+./$1
+
+find . -name "*.profraw" -exec mv {} ./ \;
+rust-profdata merge -sparse *.profraw -o default.profdata
+rm -f *.profraw
+rust-cov export $1 -instr-profile=default.profdata --format=lcov > default.lcov
+rm -f default.profdata
+
+./lcov2cobertura/target/debug/lcov2xml -d default.lcov
+mv coverage.xml "$1.xml"
+rm -f default.lcov
